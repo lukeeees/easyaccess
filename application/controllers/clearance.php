af88e31ffec155280f92be8d7e1647552daa5f3d
lukeeees <lukelaylo@gmail.com>
2016-09-30 23:22:42 +0800

    branch to master
    luke

--- application/controllers/clearance.php
+++ application/controllers/clearance.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class account extends CI_Controller {
+class clearance extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
