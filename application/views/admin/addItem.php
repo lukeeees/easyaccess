bf5db824e87320664dbe5c2486e070fd75f3bff5
RhejLorrie <rhejlorrie@gmail.com>
2016-09-30 20:34:14 +0800

    Manage Item Module

--- application/views/admin/addItem.php
+++ application/views/admin/addItem.php
@@ -6,55 +6,78 @@
 <?php
 echo @$msg;                       
 echo "<table>";
-	echo form_open('item/addItem_admin');
+	echo form_open('item_admin/ItemAdd');
 
 	echo "<tr>";
-		echo "<td>Id Number</td>";
-		echo "<td>".form_input('idnum','','class="form-control" placeholder="Id Number" required')."</td>";
+		echo "<td>Item Code</td>";
+		echo "<td>".form_input('itemCode','','class="form-control" placeholder="Item Code" required')."</td>";
 	echo "</tr>";
 
 	echo "<tr>";
-		echo "<td>Username</td>";
-		echo "<td>".form_input('name','','class="form-control" placeholder="Username" required')."</td>";
+		echo "<td>Item Name</td>";
+		echo "<td>".form_input('itemName','','class="form-control" placeholder="Item Name" required')."</td>";
 	echo "</tr>";
 
 	echo "<tr>";
-		echo "<td>Last Name</td>";
-		echo "<td>".form_input('lname','','class="form-control" placeholder="Last Name" required')."</td>";
+		echo "<td>Description</td>";
+		echo "<td>".form_input('description','','class="form-control" placeholder="Descriptiion" required')."</td>";
 	echo "</tr>";
 
 	echo "<tr>";
-		echo "<td>First Name</td>";
-		echo "<td>".form_input('fname','','class="form-control" placeholder="First Name" required')."</td>";
+		echo "<td>Previous Status</td>";
+		echo "<td>".form_input('previousStatus','','class="form-control" placeholder="Previous Status" required')."</td>";
 	echo "</tr>";
 
 	echo "<tr>";
-		echo "<td>Middle Name</td>";
-		echo "<td>".form_input('mname','','class="form-control" placeholder="Middle Name" required')."</td>";
+		echo "<td>Current Status</td>";
+		echo "<td>".form_input('currentStatus','','class="form-control" placeholder="Current Status" required')."</td>";
 	echo "</tr>";
 
 	echo "<tr>";
-		echo "<td>Password</td>";
-		echo "<td>".form_password('pass','','class="form-control" placeholder="Password" required')."<td>";
+		echo "<td>Serial Number</td>";
+		echo "<td>".form_input('serialNumber','','class="form-control" placeholder="Serial Number" required')."<td>";
 	echo "<tr>";
 
 	echo "<tr>";
-		echo "<td>Department</td>";
-		$arrayName = array('CPE'	 => 	'Computer Engineering',
-						   'CE'		 =>		'Civil Engineering',
-						   'CHE'	 =>		'Chemical Engineering',
-						   'EEE'	 =>		'Electrical and Electronics Engineering',
-						   'IE'		 =>		'Industrial Engineering',
-						   'ME'		 =>		'Mechanical Engineering');
-		echo "<td>".form_dropdown('department',$arrayName,'class="form-control" placeholder="Department"')."</td>";
-	echo "</tr>";
+		echo "<td>Part Number</td>";
+		echo "<td>".form_input('partNumber','','class="form-control" placeholder="Part Number" required')."<td>";
+	echo "<tr>";
 
 	echo "<tr>";
-		echo "<td>&nbsp;</td>";
-		echo "<td>".form_submit('submit','add user','class="form-control"');
-	echo "</tr>";
+		echo "<td>Manufacture Number</td>";
+		echo "<td>".form_input('manufactureNumber','','class="form-control" placeholder="Manufacture Number" required')."<td>";
+	echo "<tr>";
 
+	echo "<tr>";
+		echo "<td>Date Acquired</td>";
+		echo "<td>".form_input('dateAcquired','','class="form-control" placeholder="Date Acquired" required')."<td>";
+	echo "<tr>";
+	
+	echo "<tr>";
+		echo "<td>Remarks</td>";
+		echo "<td>".form_input('remarks','','class="form-control" placeholder="Remarks" required')."<td>";
+	echo "<tr>";
+
+	echo "<tr>";
+		echo "<td>Total Quantity</td>";
+		echo "<td>".form_input('totalQuantity','','class="form-control" placeholder="Total Quantity" required')."<td>";
+	echo "<tr>";
+
+	echo "<tr>";
+		echo "<td>Available Quantity</td>";
+		echo "<td>".form_input('availableQuantity','','class="form-control" placeholder="Available Quantity" required')."<td>";
+	echo "<tr>";
+
+	echo "<tr>";
+		echo "<td>Damaged Quantity</td>";
+		echo "<td>".form_input('damagedQuantity','','class="form-control" placeholder="Damaged Quantity" required')."<td>";
+	echo "<tr>";
+
+//echo "<tr><td><button type='button' class='btn-xl btn-danger'>Submit</button></td></tr>";
+	echo "<tr><td>".form_submit('submit','submit','class="form-control"')."</td></tr>";
 	echo form_close();
 echo "</table>";
+
+
 
 ?>
---
+++ application/views/admin/searchItem.php
@@ -0,0 +1,74 @@
+<div class="container">
+<?php echo br(4); ?>
+<div class="panel panel-primary">
+        <div class="panel-body">
+        <?php 
+        $attributes = array("class" => "form-horizontal", "role" => "form", "id" => "searchform", "name" => "searchform");
+        echo form_open("item_admin/ItemSearch", $attributes);?>
+            <div class="form-group">
+                <div class="col-md-6" style="margin-left:20%">
+                    <input class="form-control" id="name_search" name="name_search" placeholder="Search for Item Name" type="text" value="<?php echo set_value('name_search'); ?>" />
+                </div>
+                <div>
+                    <input id="btn_search" name="btn_search" type="submit" class="btn btn-danger" value="Search"/>
+                    <!--<a href="<?php //echo base_url(). "index.php/inventory/index"; ?>" class="btn btn-default">Show All</a> -->
+                </div>
+            </div>
+        <?php echo form_close(); ?>
+        </div>
+    </div>
+
+
+
+<div class="starter-template">
+	
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Item Code</th>
+                  <th>Item Name</th>
+                  <th>Description</th>
+                  <th>Previous Status</th>
+                  <th>Current Status</th>
+                  <th>Serial Number</th>
+                  <th>Part Number</th>
+                  <th>Manufacture Number</th>
+                  <th>Date Acquired</th>
+                  <th>Remarks</th>
+                  <th>Total Quantity</th>
+                  <th>Available Quantity</th>
+                  <th>Damaged Quantity</th>
+                </tr>
+              </thead>
+              <tbody>
+
+              <?php for ($i = 0; $i < count($x); ++$i) { ?>
+                <tr>
+                    <td><?php echo $i+1?></td>
+                    <td><?php echo $x[$i]->code; ?></td>
+                    <td><?php echo $x[$i]->name; ?></td>
+                    <td><?php echo $x[$i]->description; ?></td>
+                    <td><?php echo $x[$i]->previousstatus; ?></td>
+                    <td><?php echo $x[$i]->currentstatus; ?></td>
+                    <td><?php echo $x[$i]->serialnumber; ?></td>
+                    <td><?php echo $x[$i]->partnumber; ?></td>
+                    <td><?php echo $x[$i]->manufacturenumber; ?></td>
+                    <td><?php echo $x[$i]->dateacquired; ?></td>
+                    <td><?php echo $x[$i]->remarks; ?></td>
+                    <td><?php echo $x[$i]->totalquantity; ?></td>
+                    <td><?php echo $x[$i]->availablequantity; ?></td>
+                    <td><?php echo $x[$i]->damagedquantity; ?></td>
+
+                    
+                </tr>
+                <?php } ?>
+
+              </tbody>
+            </table>
+          </div>
+   </div>
+</div><!-- /.container -->
+
+
